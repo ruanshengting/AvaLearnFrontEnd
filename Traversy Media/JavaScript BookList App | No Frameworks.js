8be1ffef15eba9b24e30e930ab3c7a4234c6bef5
ava <869161652@qq.com>
2022-04-24 20:58:48 +0800

    UI.showAlert('already Added', 'success')

--- Traversy Media/JavaScript BookList App | No Frameworks.js
+++ Traversy Media/JavaScript BookList App | No Frameworks.js
@@ -36,16 +36,56 @@ class UI {
       <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`
     list.appendChild(row)
   }
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove()
+      UI.showAlert('already removed', 'success')
+    }
+  }
+  static clearBook() {
+    document.querySelector('#title').value = ''
+    document.querySelector('#author').value = ''
+    document.querySelector('#isbn').value = ''
+  }
+  static showAlert(message, className) {
+    const div = document.createElement('div')
+    div.className = `alert alert-${className}`
+    //div.textContent = message
+    div.appendChild(document.createTextNode(message))
+    const container = document.querySelector('.container')
+
+    const form = document.querySelector('#book-form')
+    container.insertBefore(div, form) /*Insert div before form*/
+    //Vanish in 3 second
+    setTimeout(() => document.querySelector('.alert').remove(), 3000)
+  }
 }
 //Store Class:Handles Storage
 //Event:Display a book
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
 //Event:Add a Book
 document.getElementById('book-form').addEventListener('submit', (e) => {
+  e.preventDefault()
   //get form value
   const title = document.querySelector('#title').value
   const author = document.querySelector('#author').value
   const isbn = document.querySelector('#isbn').value
-  //Instatiate Book
+  if (title === '' || author === '' || isbn === '') {
+    // alert('fill all!')
+    UI.showAlert('fill all!', 'danger')
+  } else {
+    //Instatiate Book
+    const book = new Book(title, author, isbn)
+    console.log('🚀 ~ ~ book', book)
+    //Add book to UI
+    UI.addBookToList(book)
+    UI.showAlert('already Added', 'success')
+    //clear book
+    UI.clearBook()
+  }
 })
 //Eent:Remove a book
+document.getElementById('book-list').addEventListener('click', (e) => {
+  console.log(e.target)
+  UI.deleteBook(e.target)
+})
