74140b054ecfd9d2c316d63d89ff10f42a4fec13
ava <869161652@qq.com>
2022-04-24 22:52:24 +0800

    add

--- Traversy Media/promise.js
+++ Traversy Media/promise.js
@@ -13,9 +13,18 @@ function getPosts() {
   }, 1000) //延迟1000ms后执行这个方法
 }
 function createPost(post) {
-  setTimeout(() => {
-    posts.push(post)
-  }, 2000)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post)
+      const error = false
+      //const error = true
+      if (!error) {
+        resolve()
+      } else {
+        reject('Error!')
+      }
+    }, 2000)
+  })
 }
 /**
  * 调用这2个方法
@@ -24,7 +33,16 @@ function createPost(post) {
    当createPost的方法比getPosts花的时间久我们就看不到新的post了
  */
 
-createPost({ title: 'post 3', body: 'this is post 3' }, getPosts) //处理异步
-/*
-当createPost的方法比getPosts花的时间久我们就看不到新的post了
-*/
+createPost({ title: 'post 3', body: 'this is post 3' })
+  .then(getPosts)
+  .catch((err) => console.log(err))
+//处理异步
+
+//Promise all
+const promise1 = Promise.resolve('Hello World')
+const promise2 = 10
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 2000, 'Goodbye')
+})
+Promise.all([promise1, promise2, promise3]).then((value) => console.log(value))
+//用Promise.all可以避免重复写.then().then()
