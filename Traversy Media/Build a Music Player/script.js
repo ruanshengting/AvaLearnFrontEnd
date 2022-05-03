a3e1470b8bc8afb173a84427c7484fe4caddfff6
ava <869161652@qq.com>
2022-05-03 22:55:51 +0800

    add

--- Traversy Media/Build a Music Player/script.js
+++ Traversy Media/Build a Music Player/script.js
@@ -10,12 +10,28 @@ const cover = document.querySelector('#cover')
 //Song titles
 const songs = ['Reverse', 'testMusic', 'You Nuan Chi', 'You']
 //Keep track of songs
-let songIndex = 3
+let songIndex = 0
 //Initailly load song info Dom
 loadSong(songs[songIndex])
 function loadSong(song) {
   console.log(song)
+  const title = document.querySelector('#title')
   title.innerHTML = song
   audio.src = `music/${song}.mp3`
   cover.src = `image/${song}.png`
 }
+
+//event listener
+playBtn.addEventListener('click', () => {
+  const isPlay = musicContainer.classList.contains('play')
+  if (isPlay) {
+    pauseSong()
+  } else {
+    playSong()
+  }
+})
+function playSong() {
+  console.log('12345')
+  musicContainer.classList.add('play')
+}
+function pauseSong() {}
