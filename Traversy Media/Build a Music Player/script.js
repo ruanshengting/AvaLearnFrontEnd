fc3950da48b98599374d58ac941972b17de43f60
ava <869161652@qq.com>
2022-05-04 16:47:41 +0800

    add all

--- Traversy Media/Build a Music Player/script.js
+++ Traversy Media/Build a Music Player/script.js
@@ -10,17 +10,27 @@ const cover = document.querySelector('#cover')
 //Song titles
 const songs = ['Reverse', 'testMusic', 'You Nuan Chi', 'You']
 //Keep track of songs
-let songIndex = 0
+let songIndex = 2
 //Initailly load song info Dom
 loadSong(songs[songIndex])
 function loadSong(song) {
   console.log(song)
-  const title = document.querySelector('#title')
   title.innerHTML = song
   audio.src = `music/${song}.mp3`
   cover.src = `image/${song}.png`
 }
-
+function updateProgress(e) {
+  const { currentTime, duration } = e.srcElement
+  const progressPercent = (currentTime / duration) * 100
+  progress.style.width = `${progressPercent}%`
+}
+function setProgress(e) {
+  const width = this.clientWidth
+  const clientX = e.offsetX
+  console.log('🚀 setProgress ~ width,clientX', width, clientX)
+  const duration = audio.duration
+  audio.currentTime = (clientX / width) * duration
+}
 //event listener
 playBtn.addEventListener('click', () => {
   const isPlay = musicContainer.classList.contains('play')
@@ -31,7 +41,30 @@ playBtn.addEventListener('click', () => {
   }
 })
 function playSong() {
-  console.log('12345')
   musicContainer.classList.add('play')
+  playBtn.querySelector('i.fas').classList.remove('fa-play')
+  playBtn.querySelector('i.fas').classList.add('fa-pause')
+  audio.play()
 }
-function pauseSong() {}
+function pauseSong() {
+  musicContainer.classList.remove('play')
+  playBtn.querySelector('i.fas').classList.add('fa-play')
+  playBtn.querySelector('i.fas').classList.remove('fa-pause')
+  audio.pause()
+}
+function prevSong() {
+  songIndex <= 0 ? (songIndex = songs.length - 1) : songIndex--
+  loadSong(songs[songIndex])
+  playSong()
+}
+function nextSong() {
+  songIndex >= songs.length - 1 ? (songIndex = 0) : songIndex++
+  loadSong(songs[songIndex])
+  playSong()
+}
+//change songs
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+audio.addEventListener('timeupdate', updateProgress)
+progressContainer.addEventListener('click', setProgress)
+audio.addEventListener('ended', nextSong)
