afe83bb07e097bde6a2e1a4892cdd03d35ec54c8
ava <869161652@qq.com>
2022-05-17 10:28:42 +0800

    add

---
+++ Traversy Media/Password Generator/main.js
@@ -0,0 +1,6 @@
+function getRandomSymbol() {
+  const symbol = '!#$%()+,.&~'
+  console.log('symbol.length:', symbol.length)
+  return symbol[Math.floor(Math.random() * symbol.length)]
+}
+console.log('...', getRandomSymbol())
