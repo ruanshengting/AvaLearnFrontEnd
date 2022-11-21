01860520ad53ef3b52be4661d97f0a7d90cb90ca
ava <869161652@qq.com>
2022-11-21 16:00:05 +0800

    324234

--- 第十五章 你应该学习的 JS 异步编程与Promise/ajax.js
+++ 第十五章 你应该学习的 JS 异步编程与Promise/ajax.js
@@ -17,10 +17,11 @@ function ajax(url, callback) {
 //14 使用Promise封装AJAX异步请求
 function ajax(url) {
   return new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest()
-    xhr.open('GET', url)
-    xhr.send()
+    let xhr = new XMLHttpRequest() //基于 new 出来的 XMLHttpRequest 实例对象, 可以发起 Ajax 的请求。
+    xhr.open('GET', url) //调用 open() 方法创建请求, xhr.open('请求方式', '请求路径'...)
+    xhr.send() // 调用 send() 发送请求
     xhr.onload = function () {
+      //onload: 接收得到的数据
       if (this.status == 200) {
         resolve(JSON.parse(this.response))
       } else {
